f5651f45021165797ccf94c1759a32ee12c905bd
azertypow <azertypow@gmail.com>
2018-05-28 12:17:20 +0200

    generator performance (don't use "innerHtml/innerText")

--- src/client/window-generator/main.ts
+++ src/client/window-generator/main.ts
@@ -11,10 +11,9 @@ ipcRenderer.on("message", (event: Event, message: string) => {
 });
 
 const page = document.createElement("div");
-page.style.width = "100px";
+page.style.width = "1280px";
 page.style.border = "solid";
 page.style.borderColor = "black";
-page.innerHTML = loremIpsum();
 document.body.appendChild(page);
 
 const reply = {
@@ -27,4 +26,41 @@ const reply = {
 // https://medium.com/@kahlil/how-to-communicate-between-two-electron-windows-166fdbcdc469
 ipcRenderer.send("reply", reply);
 
-const canvas = document.createElement("canvas");
+const pages: Array<string> = [];
+
+loopWord(0, loremIpsum().split(" "), "", page, 500);
+
+function loopWord(index: number, words: Array<string>, text: string, textContainer: HTMLDivElement, height: number) {
+
+    const time = new Date().getTime();
+
+    while (index < words.length) {
+        // textContainer.innerText = text.length > 0 ? `${text} ${words[index]}` : words[index];
+        const span = document.createElement("span");
+        span.innerHTML = text.length > 0 ? ` ${words[index]}` : words[index];
+        textContainer.insertBefore(span, null);
+
+        if (textContainer.getBoundingClientRect().height > height) {
+            while (textContainer.firstChild) {
+                textContainer.removeChild(textContainer.firstChild);
+            }
+            pages.push(text);
+            text  = words[index];
+        } else {
+            text = text.length > 0 ? `${text} ${words[index]}` : words[index];
+        }
+        index++;
+    }
+
+    console.log(new Date().getTime() - time);
+    console.log(pages);
+
+    for(const page of pages){
+        const pageContainer = document.createElement("div");
+        pageContainer.innerHTML = page;
+        pageContainer.style.border = "solid 1px black";
+        pageContainer.style.width = "1280px";
+        document.body.insertBefore(pageContainer, document.body.firstChild);
+    }
+    console.log(new Date().getTime() - time);
+}
